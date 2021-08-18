e5646dfcf6f2cfd50187bac4db79a37569418af6
soohyoen <gnejfejf789@naver.com>
2021-08-18 09:24:44 +0900

    2021.08.19 게시판 생성및 확인 버튼활성화

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView, ProjectDetailView
+from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
 app_name = 'projectapp'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
 
     path('detail/<int:pk>',ProjectDetailView.as_view(), name='detail'),
+
+    path('list/',ProjectListView.as_view(), name='list')
 ]
