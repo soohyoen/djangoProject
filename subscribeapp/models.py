1ce077a7351507a44a4001677b596689b339b70d
soohyoen <gnejfejf789@naver.com>
2021-08-19 15:07:47 +0900

    2021.08.19 Create subcribeapp in models

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subcription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subcription',null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subcription',null=False)
+
+    class Meta:
+        unique_together = ['user', 'project']
