3cf40c9d2d3b461c90969f97746a29d9ebb1437c
soohyoen <gnejfejf789@naver.com>
2021-08-23 15:43:44 +0900

    2021.08.23 create likeapp

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path('article/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
+
     path('subcribe/',include('subscribeapp.urls')),
+    path('likes/',include('likeapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
